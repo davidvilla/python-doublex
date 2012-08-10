0edbc79f364f524ad4d9dfbcc38c4cf4ccc4505c
David Villa Alises <838459+davidvilla@users.noreply.github.com>
2012-08-10 10:45:01 +0200

    basic spy support with collaborator verification

--- doublex/__init__.py
+++ doublex/__init__.py
@@ -1,37 +1,140 @@
 # -*- coding:utf-8; tab-width:4; mode:python -*-
 
+import types
+import inspect
+
 from hamcrest.core.base_matcher import BaseMatcher
+
+import safeunicode
 
 
 class Spy(object):
     def __init__(self, collaborator=None):
-        self.collaborator = collaborator
+        self.proxy = create_collaborator_proxy(collaborator)
+        self.invocations = []
 
-    def assert_match_collaborator(self, key):
-        if self.collaborator is not None and not hasattr(self.collaborator, key):
-            raise ApiMismatch()
+    def add_invocation(self, invocation):
+        self.proxy.assert_match_signature(invocation)
+        self.invocations.append(invocation)
+
+    def was_called(self, invocation, times):
+        return self.invocations.count(invocation) >= times
 
     def __getattr__(self, key):
-        self.assert_match_collaborator(key)
+        self.proxy.assert_has_method(key)
 
-        method = Method(key)
+        method = Method(self, key)
         setattr(self, key, method)
         return method
 
 
+def create_collaborator_proxy(collaborator):
+    if collaborator is None:
+        return DummyCollaboratorProxy()
+
+    return CollaboratorProxy(collaborator)
+
+
+class DummyCollaboratorProxy(object):
+    def assert_has_method(self, name):
+        pass
+
+    def assert_match_signature(self, invocation):
+        pass
+
+
+class CollaboratorProxy(object):
+    def __init__(self, collaborator):
+        self.collaborator = self.get_class(collaborator)
+
+    def get_class(self, something):
+        if isinstance(something, (types.ClassType, type)):
+            return something
+
+        if isinstance(something, (types.InstanceType, object)):
+            return something.__class__
+
+        raise Exception("collaborator must be class or instance")
+
+    def assert_match_signature(self, invocation):
+        self.assert_has_method(invocation.name)
+        method = getattr(self.collaborator, invocation.name)
+        argspec = inspect.getargspec(method)
+
+#        print method.__class__
+
+#        print len(invocation.context.args), len(argspec.args)
+#        print "args:", argspec.args
+#        print "varargs:", argspec.varargs
+#        print "keywords:", argspec.keywords
+#        print "defaults:", argspec.defaults
+
+        positionals = self.count_positionals(argspec)
+        keywords = self.get_keywords(argspec)
+
+#        print keywords
+
+        if len(invocation.context.args) != positionals:
+            reason = "Mismatching positional arguments:\n%s" %\
+                self.add_class_prefix(invocation.name,
+                                      inspect.formatargspec(*argspec))
+            raise ApiMismatch(reason)
+
+#        print invocation, argspec
+
+    def count_positionals(self, argspec):
+        if argspec.defaults is None:
+            ndefaults = 0
+        else:
+            ndefaults = len(argspec.defaults)
+
+        return len(argspec.args) - ndefaults - 1
+
+    def get_keywords(self, argspec):
+        if argspec.defaults is None:
+            return []
+
+        return argspec.args[-len(argspec.defaults):]
+
+    def assert_has_method(self, name):
+        if not hasattr(self.collaborator, name):
+            reason = "No such method: %s" % self.add_class_prefix(name)
+            raise ApiMismatch(reason)
+
+    def add_class_prefix(self, name, args=''):
+        return "%s.%s%s" % (self.collaborator.__name__, name, args)
+
+
 class Method(object):
-    def __init__(self, name):
+    def __init__(self, double, name):
+        self.double = double
         self.name = name
-        self.invocations = []
 
     def __call__(self, *args, **kargs):
-        self.invocations.append(Invocation(*args, **kargs))
+        self.double.add_invocation(
+            Invocation(self.name, InvocationContext(*args, **kargs)))
+
+    def was_called(self, context, times):
+        return self.double.was_called(
+            Invocation(self.name, context), times)
 
     def __repr__(self):
         return "Method(%s)" % self.name
 
 
 class Invocation(object):
+    def __init__(self, name, context):
+        self.name = name
+        self.context = context
+
+    def __eq__(self, other):
+        return (self.name, self.context) == (other.name, other.context)
+
+    def __repr__(self):
+        return "%s%s" % (self.name, self.context)
+
+
+class InvocationContext(object):
     def __init__(self, *args, **kargs):
         self.args = args
         self.kargs = kargs
@@ -43,19 +146,19 @@ class Invocation(object):
         if self.any_arg() or other.any_arg():
             return True
 
-        return self.args == other.args and self.kargs == other.kargs
+        return (self.args, self.kargs) == (other.args, other.kargs)
 
-    def __repr__(self):
-        return "Invocation(%s, %s)" % (self.args, self.kargs)
+    def __str__(self):
+        return str(InvocationFormatter(self.args, self.kargs))
 
 
 class MethodCalled(BaseMatcher):
-    def __init__(self, invocation, times=1):
-        self.invocation = invocation
+    def __init__(self, context, times=1):
+        self.context = context
         self._times = times
 
     def _matches(self, method):
-        return method.invocations.count(self.invocation) >= self._times
+        return method.was_called(self.context, self._times)
 
     def describe_to(self, description):
         description.append_text('method called ')
@@ -63,15 +166,15 @@ class MethodCalled(BaseMatcher):
             description.append_text('%s times ' % self._times)
 
     def times(self, n):
-        return MethodCalled(self.invocation, times=n)
+        return MethodCalled(self.context, times=n)
 
 
 def called():
-    return MethodCalled(Invocation(ANY_ARG))
+    return MethodCalled(InvocationContext(ANY_ARG))
 
 
 def called_with(*args, **kargs):
-    return MethodCalled(Invocation(*args, **kargs))
+    return MethodCalled(InvocationContext(*args, **kargs))
 
 
 ANY_ARG = "__ANY_ARG__"
@@ -79,3 +182,41 @@ ANY_ARG = "__ANY_ARG__"
 
 class ApiMismatch(Exception):
     pass
+
+
+class InvocationFormatter(object):
+    def __init__(self, args, kargs):
+        self.args = args
+        self.kargs = kargs
+        self.output = None
+
+    def __str__(self):
+        arg_values = []
+        if self.args:
+            arg_values.append(self._format_args(self.args))
+        if self.kargs:
+            arg_values.append(self._format_kargs(self.kargs))
+
+        return "(%s) -> %s" % (str.join(', ', arg_values),
+                               repr(self.output))
+
+    @staticmethod
+    def _format_args(args):
+        str_args = []
+        for arg in args:
+            if isinstance(arg, unicode):
+                arg = safeunicode.get_string(arg)
+
+            if isinstance(arg, (int, str, dict)):
+                str_args.append(repr(arg))
+            else:
+                str_args.append(str(arg))
+
+        return str.join(', ', str_args)
+
+    @staticmethod
+    def _format_kargs(kargs):
+        items = ['%s=%s' % (key, repr(val))
+                 for key, val in sorted(kargs.items())]
+
+        return str.join(', ', items)
--- test/unit.py
+++ test/unit.py
@@ -2,7 +2,7 @@
 
 from unittest import TestCase
 
-from hamcrest import *
+from hamcrest import assert_that, is_not
 from doublex import Spy, called, called_with, ApiMismatch, ANY_ARG
 
 
@@ -82,9 +82,71 @@ class EmptySpyTests(TestCase):
         assert_that(self.spy.foo, called().times(3))
 
 
+class VerifiedSpyTests(TestCase):
+    def setUp(self):
+        self.spy = Spy(Collaborator())
+
+    def test_call_unexisting_method(self):
+        try:
+            self.spy.wrong()
+            self.fail('ApiMismatch should be raised')
+        except ApiMismatch as e:
+            self.assertIn(str(e), "No such method: Collaborator.wrong")
+
+    def test_check_unexisting_method(self):
+        try:
+            assert_that(self.spy.wrong, called())
+            self.fail('ApiMismatch should be raised')
+        except ApiMismatch as e:
+            self.assertIn(str(e), "No such method: Collaborator.wrong")
+
+    def test_create_from_oldstyle_class(self):
+        self.spy = Spy(Collaborator)
+
+    def test_create_from_newstyle_class(self):
+        class Actor(object):
+            pass
+
+        self.spy = Spy(Actor)
+
+
+
+
+#----------------------------#
+#- pyDoubles migrated tests -#
+#----------------------------#
+
 class Collaborator:
+    """
+    The original object we double in tests
+    """
+    test_field = "OK"
+
     def hello(self):
         return "hello"
+
+    def something(self):
+        return "ok"
+
+    def one_arg_method(self, arg1):
+        return arg1
+
+    def two_args_method(self, arg1, arg2):
+        return arg1 + arg2
+
+    def kwarg_method(self, key_param=False):
+        return key_param
+
+    def mixed_method(self, arg1, key_param=False):
+        return key_param + arg1
+
+    def void_method(self):
+        pass
+
+    def method_one(self, arg1):
+        return 1
+
+    alias_method = one_arg_method
 
 
 class pyDoublesSpyTests(TestCase):
@@ -106,33 +168,36 @@ class pyDoublesSpyTests(TestCase):
         self.spy.hello()
         assert_that(self.spy.hello, called())
 
-#    def test_spy_based_on_object_must_check_api_match(self):
-#        try:
-#            self.spy.hello("unexpected argument")
-#            self.fail('Expection should raise: Actual objet does not accept parameters')
-#        except ApiMismatch:
-#            pass
-#
-#    def test_check_api_match_with_kwargs(self):
-#        self.assertTrue(self.spy.mixed_method(1, key_param=2) is None)
-#
-#    def test_check_api_match_with_kwargs_not_used(self):
-#        self.assertTrue(self.spy.mixed_method(1) is None)
-#
-#    def test_check_api_match_with_kwargs_not_matching(self):
-#        try:
-#            self.spy.mixed_method(1, 2, 3)
-#            self.fail('Api mismatch not detected')
-#        except ApiMismatch:
-#            pass
-#
-#    def test_match_call_with_unicode_and_non_ascii_chars(self):
-#        non_ascii  = u'España'
-#        self.spy.one_arg_method(non_ascii)
-#
+    def test_spy_based_on_object_must_check_api_match(self):
+        try:
+            self.spy.hello("unexpected argument")
+            self.fail('Expection should raise: Actual objet does not accept parameters')
+        except ApiMismatch:
+            pass
+
+    def test_check_api_match_with_kwargs(self):
+        self.assertTrue(self.spy.mixed_method(1, key_param=2) is None)
+
+    def test_check_api_match_with_kwargs_not_used(self):
+        self.assertTrue(self.spy.mixed_method(1) is None)
+
+    def test_check_api_match_with_kwargs_not_matching(self):
+        try:
+            self.spy.mixed_method(1, 2, 3)
+            self.fail('Api mismatch not detected')
+        except ApiMismatch:
+            pass
+
+    def test_match_call_with_unicode_and_non_ascii_chars(self):
+        non_ascii  = u'España'
+        self.spy.one_arg_method(non_ascii)
+
+        assert_that(self.spy.one_arg_method, called_with(non_ascii))
+
+#        pyDoubles:
 #        assert_that_was_called(self.spy.one_arg_method).with_args(
 #                                                        non_ascii)
-#
+
 #    def test_stub_methods_can_be_handled_separately(self):
 #        when(self.spy.one_arg_method).with_args(1).then_return(1000)
 #        when(self.spy.two_args_method).with_args(5, 5).then_return(2000)
@@ -143,11 +208,12 @@ class pyDoublesSpyTests(TestCase):
 #
 #        assert_that_was_called(handle1).with_args(1)
 #        assert_that_was_called(handle2).with_args(5, 5)
-#
+
+#    #SAME as VerifiedSpyTests.test_check_unexisting_method
 #    def test_assert_was_called_with_method_not_in_the_api(self):
 #        self.failUnlessRaises(ApiMismatch,
 #            assert_that_was_called, self.spy.unexisting_method)
-#
+
 #    def test_do_not_call_callable_object_if_wasnt_generated_by_the_framework(self):
 #        class CallableObj():
 #            just_testing = True
@@ -160,9 +226,3 @@ class pyDoublesSpyTests(TestCase):
 #
 #        self.assertEquals(obj, self.spy.one_arg_method(1),
 #                       "Wrong returned object")
-#
-
-# class DoublexVerifiedSpyTests(TestCase):
-# TODO
-#    def test_create_from_class_too(self):
-#        self.spy = Spy(Collaborator)
