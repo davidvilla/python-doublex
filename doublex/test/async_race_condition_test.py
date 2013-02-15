39500a3e43d6429832e25dd707df28dd290c110f
David Villa Alises <838459+davidvilla@users.noreply.github.com>
2013-02-15 03:11:48 +0100

    non intrusive race test using a ProxySpy

--- doublex/test/async_race_condition_test.py
+++ doublex/test/async_race_condition_test.py
@@ -6,22 +6,12 @@ import time
 import thread
 import unittest
 
-from doublex import Spy, assert_that, called
-
-
-def elapser(fn):
-    def deco(*args, **kwargs):
-        time.sleep(0.2)
-        return fn(*args, **kwargs)
-    return deco
-
-from doublex.internal import Method
-Method.create_invocation = elapser(Method.create_invocation)
+from doublex import ProxySpy, assert_that, called
 
 
 class Collaborator(object):
     def write(self, data):
-        print "your code here"
+        time.sleep(0.3)
 
 
 class SUT(object):
@@ -33,17 +23,17 @@ class SUT(object):
         self.collaborator.write("something")
 
     def some_method(self):
-        thread.start_new_thread(self.delayed_write, tuple())
+        thread.start_new_thread(self.delayed_write, ())
 
 
 class AsyncTests(unittest.TestCase):
     def test_wrong_try_to_test_an_async_invocation(self):
         # given
-        spy = Spy(Collaborator)
+        spy = ProxySpy(Collaborator())
         sut = SUT(spy)
 
         # when
         sut.some_method()
 
         # then
-        assert_that(spy.write, called().async(5))
+        assert_that(spy.write, called().async(1))
