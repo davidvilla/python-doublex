7796b372dae5df26dfff798d775a3669eba457b0
David Villa Alises <838459+davidvilla@users.noreply.github.com>
2013-04-25 09:14:49 +0200

    Added tag v1.6.5 for changeset 1094f91a921b

---
+++ debian-vcs.mk
@@ -0,0 +1,24 @@
+#!/usr/bin/make -f
+# -*- mode:makefile -*-
+
+URL_AUTH=svn+ssh://${ALIOTH_USER}@svn.debian.org/svn/python-modules/packages/doublex/trunk
+URL_ANON=svn://svn.debian.org/svn/python-modules/packages/doublex/trunk
+
+debian:
+	if [ ! -z "$${ALIOTH_USER}" ]; then \
+	    svn co ${URL_AUTH} -N; \
+	else \
+	    svn co ${URL_ANON} -N; \
+	fi
+
+	mv trunk/.svn .
+	rmdir trunk
+	svn up debian
+
+clean:
+	find . -name *.pyc -delete
+	find . -name *.pyo -delete
+	find . -name *~ -delete
+	$(RM) -r dist build *.egg-info
+	$(RM) -r .svn debian MANIFEST
+	$(RM) -r *.egg-info
